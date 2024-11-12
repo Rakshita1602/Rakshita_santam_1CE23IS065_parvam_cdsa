0048d43b1efc203db65ddf78467becebbb5af434
Rakshita1602 <rakshitasantam16@gmail.com>
2024-11-12 09:34:36 +0530

    Add files via upload

---
+++ primeNo.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main(){
+    for(i=0;i<=11;i++){
+        if(11%2){
+            printf("It is a prime number");
+        }
+        else
+        printf("It is not a prime number");
+    }
+}
